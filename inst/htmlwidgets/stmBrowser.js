459197bba7fe155c5283ee23bae3187dcf28e436
timelyportfolio <kent.russell@timelyportfolio.com>
2015-03-31 14:53:11 -0500

    incremental commit in progress toward htmlwidget; get js/css dependencies

---
+++ inst/htmlwidgets/stmBrowser.js
@@ -0,0 +1,25 @@
+HTMLWidgets.widget({
+
+  name: 'stmBrowser',
+
+  type: 'output',
+
+  initialize: function(el, width, height) {
+    
+    return {
+      // TODO: add instance fields as required
+    }
+
+  },
+
+  renderValue: function(el, x, instance) {
+    
+
+  },
+
+  resize: function(el, width, height, instance) {
+
+  }
+  
+
+});
